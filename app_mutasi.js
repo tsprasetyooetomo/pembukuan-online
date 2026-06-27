d352dfb60c50fe921bb979578e848d74ea4c732a
tsprasetyooetomo <ts.prasetyooetomo@gmail.com>
2026-06-27 10:58:34 +0700

    update mutasi

--- app_mutasi.js
+++ app_mutasi.js
@@ -1533,7 +1533,7 @@ async function addKasirDetil() {
       cr: 0,
     };
 
-    await db.add("mutasiKasir", newDetil);
+    await db.add("mutasikasir", newDetil);
 
     if (!DBCache.mutasiKasir) DBCache.mutasiKasir = [];
     DBCache.mutasiKasir.push(newDetil);
@@ -1622,7 +1622,7 @@ function renderKasirDetilTable() {
 async function hapusKasirDetil(id) {
   if (!confirm("Yakin hapus detil ini?")) return;
 
-  await db.del("mutasiKasir", id);
+  await db.del("mutasikasir", id);
 
   if (DBCache.mutasiKasir) {
     DBCache.mutasiKasir = DBCache.mutasiKasir.filter(function (t) {
