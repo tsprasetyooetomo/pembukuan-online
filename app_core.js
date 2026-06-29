4974f29cc6116b500b11be9a3fc0e441b56a1d5a
tsprasetyooetomo <ts.prasetyooetomo@gmail.com>
2026-06-29 16:05:17 +0700

    update core

--- app_core.js
+++ app_core.js
@@ -781,8 +781,10 @@ async function navigate(id) {
   });
 
   $("tbTitle").innerHTML =
-    '<i class="fa-solid ' + (m ? m.icon : "fa-home") + '"></i> ';
-  +(m ? m.label : "Dashboard");
+    '<i class="fa-solid ' +
+    (m ? m.icon : "fa-home") +
+    '"></i> ' +
+    (m ? m.label : "Dashboard");
 
   $("sidebar").classList.remove("open");
 
