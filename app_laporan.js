76a7dd1f0fa2c4e83de2e425f6312b6deea6e1b7
tsprasetyooetomo <ts.prasetyooetomo@gmail.com>
2026-07-01 14:59:28 +0700

    update laporan

--- app_laporan.js
+++ app_laporan.js
@@ -3861,18 +3861,12 @@ async function terapkanOpsiRLLebar() {
         buatBarisSubtotal(ket, arrSub, totalSub, "#1b5e20", false);
 
         // LOGIKA BARU: Simpan akumulasi Laba/Rugi sebelum reset
-
-        // Perbaikan bagian penambahan & pengurangan agar anti-NaN
         for (var b = 1; b <= 12; b++) {
           var bsLaba = ("0" + b).slice(-2);
-          var nilaiSubBulanIni = subTotalPerBulan[bsLaba] || 0; // Proteksi angka kosong
-
           if (currentDigit === "3") {
-            akumulasiLabaRugiPerBulan[bsLaba] = nilaiSubBulanIni;
+            akumulasiLabaRugiPerBulan[bsLaba] = subTotalPerBulan[bsLaba];
           } else {
-            // Proteksi juga jika nilai akumulasi sebelumnya belum terdefinisi
-            akumulasiLabaRugiPerBulan[bsLaba] =
-              (akumulasiLabaRugiPerBulan[bsLaba] || 0) + nilaiSubBulanIni;
+            akumulasiLabaRugiPerBulan[bsLaba] = subTotalPerBulan[bsLaba];
           }
         }
 
